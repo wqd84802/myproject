6514052c79649d3bb3328b3957c043e59d765855
wqd84802 <wqd84802@gmail.com>
2018-07-01 23:56:34 +0800

    这是晚上的一个更新项目

--- boards/urls.py
+++ boards/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'boards'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('board/<int:pk>/', views.board_topics, name='board_topics'),
+    path('board/<int:pk>/new/', views.new_topic, name="new_topic"),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . models import Board
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from . models import Board, Topic, Post
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
     return render(request,'boards/home.html',{"boards":boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, id=pk)
+    return render(request, 'boards/topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == "POST":
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()
+        print(user)
+        topic = Topic.objects.create(subject=subject,
+                                     board=board,
+                                     starter=user
+                                     )
+
+        post = Post.objects.create(message=message,
+                                   topic=topic,
+                                   created_by=user
+                                   )
+
+        return redirect('boards:board_topics',pk=board.pk)
+    return render(request, 'boards/new_topic.html', {'board': board})
